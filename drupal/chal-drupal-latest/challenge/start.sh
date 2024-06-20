4d2deb0ba292a8758cd48f9f55fbb6c396b25430
jp-ag <jpaguinsky@gmail.com>
2024-06-20 13:47:54 +0000

    test drupal latest

---
+++ drupal/chal-drupal-latest/challenge/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -r /sites/* /opt/drupal/web/sites/
+
+#sqlite3 /opt/drupal/web/sites/default/files/.ht.sqlite "UPDATE users SET password_hash = '$PASSWORD' WHERE id = 1;"
+apache2-foreground > /dev/null 2>&1 &
+
+# Proxy stdin/stdout to server
+socat - TCP:127.0.0.1:8080,forever
