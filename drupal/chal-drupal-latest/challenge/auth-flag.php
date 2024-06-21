d5fec405d31dd15cb91f29ca8f70e4578a27f17b
jp-ag <jpaguinsky@gmail.com>
2024-06-21 11:16:52 +0000

    drupal latest

---
+++ drupal/chal-drupal-latest/challenge/auth-flag.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\Component\Utility\Html;
+use \Drupal\node\Entity\Node;
+
+$nodes = Node::loadMultiple();
+foreach($nodes as $node){
+   $node->set('title', 'abc');
+   $node->save();
+}
+
+#drush php-script script2.php
